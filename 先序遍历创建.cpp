4a3254c5ebfdce0f98c131e5defba6d1e31b401c
yueguang23 <2216108645@qq.com>
2018-12-10 22:41:25 +0800

    xianxubianlichuangjian

---
+++ 先序遍历创建.cpp
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define MAX 50
+typedef struct Node
+{
+	char data;
+	struct Node *Lchild;
+	struct Node *Rchild;
+}*BiTree,BiTNode;
+void CreateBiTree(BiTree * root)
+{
+	char ch;
+	scanf("%c",&ch);
+	if(ch=='#')
+	*root=NULL;
+	else
+	{
+		*root=(BiTree)malloc(sizeof(BiTNode));
+	    (*root)->data=ch;
+		CreateBiTree(&((*root)->Lchild));
+		CreateBiTree(&((*root)->Rchild));
+	}
+} 
+
+//先序遍历 
+void xianxu(BiTree root) 
+{
+	if(root==NULL)
+	return;
+	printf("%c",root->data);
+	xianxu(root->Lchild);
+	xianxu(root->Rchild);
+}
+//中序 
+void zhongxu(BiTree root)
+{
+	if(root==NULL)
+	return;
+	zhongxu(root->Lchild);
+	printf("%c",root->data);
+	zhongxu(root->Rchild);
+}
+
+//后序
+void houxu(BiTree root)
+{
+	if(root==NULL)
+	return;
+	houxu(root->Lchild);
+	houxu(root->Rchild);
+	printf("%c",root->data);
+} 
+
+int main(void)
+{
+	
+	BiTree root=NULL;
+	CreateBiTree(&root);
+	xianxu(root);
+	printf("\n");
+	zhongxu(root);
+	printf("\n");
+	houxu(root);
+}
